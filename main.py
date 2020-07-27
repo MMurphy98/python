85d88c3e01e9cb4b1bc1c3b63fcbe82affbb4b65
MMurphy98 <mjg654654@outlook.com>
2020-07-27 09:31:36 +0800

    my first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+
+for letter in 'Python':     # 第一个实例
+   print('letter :', letter)
+ 
+fruits = ['banana', 'apple',  'mango']
+for fruit in fruits:        # 第二个实例
+   print('Fruits :', fruit)
+ 
